b065056950f8bc6d31d8f489930af77547db7d93
Siurob <ing.asiurob@gmail.com>
2018-11-26 17:30:38 -0600

    Prueba con heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Hulk' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'Ant Man' },
+  { id: 5, name: 'Spider Man' }
+];
